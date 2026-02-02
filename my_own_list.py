467cc5a793dbf3b977d70d0f4df7ad5d04cce8de
MZaragoza-Ramos <maximilianozaragozar@students.abtech.edu>
2026-02-02 14:38:23 -0500

    Add files via upload

---
+++ my_own_list.py
@@ -0,0 +1,6 @@
+#List containing vehicles
+vehicles = ['car', 'bus', 'subway']
+#Printed sentences involving vehicles from the list
+print(f"I would like to get my driver's license to drive my {vehicles[0]} around the parkway.")
+print(f"Taking the ART {vehicles[1]} is a good way to get around Asheville.")
+print(f"When traveling around New York, the {vehicles[2]} is very helpful.")
