dfb648b6dfbb1a8ba1ebbdad14d85ce4396e4db1
MZaragoza-Ramos <maximilianozaragozar@students.abtech.edu>
2026-02-11 12:46:40 -0500

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,6 @@
+#List of animals
+animals = ['meerkat', 'red panda', 'raccoon']
+#A For loop for that animals
+for animal in animals:
+    print(f"A {animal} has a dark circle of fur around its eyes.")
+print("\nAll these animals have a dark circle of fur around their eyes.")
